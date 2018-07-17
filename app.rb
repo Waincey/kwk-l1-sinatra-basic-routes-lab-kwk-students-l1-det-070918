5f129aa06520cd6458474585bdca9c825733296d
Waincey <waincey.chan@gmail.com>
2018-07-17 19:14:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,16 +1,16 @@
 # Sinatra Basic Routes Lab
-# require_relative 'config/environment'
-# class App < Sinatra::Base
-#   get '/name' do
-#     "My name is _"
-#   end
-#   get '/hometown' do
-#   "My hometown is __"
-#   end 
-#   get '/favorite-song' do
-#   "My favorite-song is Skyscraper by Demi Lovato"
-#   end
-# end
+require_relative 'config/environment'
+class App < Sinatra::Base
+  get '/name' do
+    "My name is _"
+  end
+  get '/hometown' do
+  "My hometown is __"
+  end 
+  get '/favorite-song' do
+  "My favorite-song is Skyscraper by Demi Lovato"
+  end
+end
 
 
 # Practice with Dynamic Route Syntax
