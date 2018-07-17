7c101d5051a8438d7d92c93217ed3255c345bb90
Waincey <waincey.chan@gmail.com>
2018-07-17 19:15:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,9 +4,11 @@ class App < Sinatra::Base
   get '/name' do
     "My name is _"
   end
+  
   get '/hometown' do
   "My hometown is __"
   end 
+  
   get '/favorite-song' do
   "My favorite-song is Skyscraper by Demi Lovato."
   end
