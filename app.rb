a2da1c1479eea3f571cfc048f676cfbf4e8f46e3
Waincey <waincey.chan@gmail.com>
2018-07-17 19:14:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
-require_relative 'config/environment'
+# Sinatra Basic Routes Lab
+# require_relative 'config/environment'
+# class App < Sinatra::Base
+#   get '/name' do
+#     "My name is _"
+#   end
+#   get '/hometown' do
+#   "My hometown is __"
+#   end 
+#   get '/favorite-song' do
+#   "My favorite-song is Skyscraper by Demi Lovato"
+#   end
+# end
 
-class App < Sinatra::Base
-end
+
+# Practice with Dynamic Route Syntax
+# get '/hello/:id' do
+#   @name = params[:id]
+#   "Hello #{@name}!"
+# end 
